ad032ba7ea2765d72592eef63acf9a137f2b9491
jiaboshi <jiaboshi@WSWINCNHZ1893.hengtiansoft.com>
2018-01-11 14:39:05 +0800

    Signed-off-by: jiaboshi <jiaboshi@WSWINCNHZ1893.hengtiansoft.com>

--- src/jiaboshi/tableexport/com/Main.java
+++ src/jiaboshi/tableexport/com/Main.java
@@ -11,18 +11,23 @@ public class Main {
         Properties props = new Properties();
         InputStream in;
         try {
-            in = new FileInputStream(args[0]);
+            System.out.println(">>>>>>>>>>>>>>开始导出数据库表结构");
+            long start = System.currentTimeMillis();
+            /*in = new FileInputStream(args[0]);
             props.load(in);
             String ip = props.getProperty("jdbc.ip");
             String post = props.getProperty("jdbc.post");
             String database = props.getProperty("jdbc.database");
             String username = props.getProperty("jdbc.user");
-            String password = props.getProperty("jdbc.password");
+            String password = props.getProperty("jdbc.password");*/
             TableInfo tableInfo = new TableInfo();
-            Map<String, Object> map = tableInfo.getTableInfo(ip, post, database, username, password);
+            Map<String, Object> map = tableInfo.getTableInfo("10.6.11.25", "3306", "lse_home_dev", "root", "Hangzhou@123");
 
             ExportDatabaseInfo info = new ExportDatabaseInfo();
-            info.createDoc(map, "testDoc", "D:\\table.doc");
+            info.createDoc(map, "testDoc", "E:\\table.doc");
+            System.out.println("导出文件为: ");
+            long end = System.currentTimeMillis();
+            System.out.println(">>>>>>>>>>>>>导出结束，用时 : " + (end - start)+ " ms");
         } catch (Exception e) {
             e.printStackTrace();
         }
